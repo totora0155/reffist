f7bbcd8afdb208378ae12d52cea12494b5ce4bf9
nju33 <nju33@nmba.local>
2016-02-22 03:12:23 +0900

    Limited fn for app

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,42 +1,12 @@
-let window = null;
+import io from 'socket.io-client';
 
 chrome.browserAction.onClicked.addListener((tab) => {
   const {url} = tab;
-  const width = 320;
-  const height = 480;
-  const type = 'popup';
-
-  (async function() {
-    window = await createWindow({url, width, height, type});
-    const {id: tabId} = window.tabs[0];
-    const message = 'tabId'
-    executeScript(tabId).then(() => {
-      chrome.runtime.sendMessage({message, tabId}, () => {});
-    });
-  })();
+  let socket = io('http://localhost:53825', {
+    reconnection: false,
+  });
+  socket.emit('open', {url});
+  socket.on('disconnect', () => {
+    socket = null;
+  });
 });
-
-function createWindow(data) {
-  return new Promise((resolve) => {
-    chrome.windows.create(data, (window) => {
-      return resolve(window);
-    });
-  });
-}
-
-function executeScript(tabId) {
-  const file = 'scripts/executer.js';
-  return new Promise((resolve) => {
-    console.log(9);
-    chrome.tabs.executeScript(tabId, {
-      code: `var __reffist__tabId = ${tabId};`,
-    }, () => {
-      console.log(111);
-      chrome.tabs.executeScript(tabId, {
-        file,
-        runAt: 'document_end',
-      }, () => {return resolve()});
-    });
-
-  });
-}
