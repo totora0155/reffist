aa028bcae8c23d31f94fafd7cc3ab4c3d5779745
nju33 <nju33@nmba.local>
2016-02-27 17:28:48 +0900

    Include title in sent data

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,11 +1,11 @@
 import io from 'socket.io-client';
 
 chrome.browserAction.onClicked.addListener((tab) => {
-  const {url} = tab;
+  const {title, url} = tab;
   let socket = io('http://localhost:53825', {
     reconnection: false,
   });
-  socket.emit('open', {url});
+  socket.emit('open', {title, url});
   socket.on('disconnect', () => {
     socket = null;
   });
